cd5b0124a665c85430d51f774d0630f82f552ab7
Rudegog <matheusgf1500@hotmail.com>
2022-12-12 17:25:55 -0300

    Adicionando estilo a página de finalização

--- src/controllers/finalizarController.js
+++ src/controllers/finalizarController.js
@@ -1,7 +1,14 @@
 const finalizarController = {
-    index:(req,res) =>{
+    index: (req, res) => {
         res.render("pages/finalizar")
+    },
+    totalPag: (req, res) => {
+        let total = 0
+        for (i = 0; i < req.session.carrinho.length; i++) {
+            total = total + parseFloat(req.session.carrinho[i].preco)
+        }
+        res.render("./pages/pagamento", { total })
     }
 }
 
-module.exports =  finalizarController;
+module.exports = finalizarController;
